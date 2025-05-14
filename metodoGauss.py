bc6dfa2d1ce921243cedaa43e476a70f4990ef86
Vinix1239 <Jmariomancillas@gmail.com>
2025-05-13 19:32:45 -0700

    Codigo Version 1

--- metodoGauss.py
+++ metodoGauss.py
@@ -9,5 +9,12 @@ for i in range(3):
         elemento = float(input("numero: "))
         fila.append(elemento)
     matriz.append(fila)
-for fila in matriz:
-    print(fila)
+renglon = 0
+
+#Bucle para poder iterar sobre cada renglón de la matriz
+for i in range (renglon, renglon < 3, renglon= renglon+1):
+    
+    #Se selecciona el elemento que esta en diagonal como pivote
+    pivote = matriz[renglon, renglon]
+
+    #Toca normalizar el renglón dividiendo cada elemento por el pivote
