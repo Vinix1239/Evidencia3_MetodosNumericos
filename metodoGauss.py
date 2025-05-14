3e73a0dbc039d3be18c9e062eea2893c21ed0e28
Vinix1239 <Jmariomancillas@gmail.com>
2025-05-13 18:40:53 -0700

    Crear Matriz (Ahora si salio como esperaba)

--- metodoGauss.py
+++ metodoGauss.py
@@ -2,12 +2,12 @@
 #Pedirle al usuario numeros para crear una matriz
 matriz = []
 print("Hola :>")
-print("Ingresa 9 numeros para llenar una matriz por favor: ")
+print("Ingresa 12 numeros para crear una matriz de 3x4")
 for i in range(3):
     fila = []
-    for j in range(3):
-        fila.append(float(input("Numero: ")))
-        break
+    for j in range(4):
+        elemento = float(input("numero: "))
+        fila.append(elemento)
     matriz.append(fila)
 for fila in matriz:
     print(fila)
